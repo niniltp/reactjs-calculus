00e9f2447981cc408cad111efb8f851f2f72e4d2
niniltp <nhienphan923@gmail.com>
2020-09-26 17:38:51 +0200

    Updates

---
+++ precache-manifest.6cc003bb9d27ba2934aebcc061527685.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d000c95194bcfaccc32cfa3bceed0a",
+    "url": "/reactjs-calculus/index.html"
+  },
+  {
+    "revision": "10f8c5eaa7aced0d4199",
+    "url": "/reactjs-calculus/static/css/main.49e29d48.chunk.css"
+  },
+  {
+    "revision": "5434e10c0e01a96fbb01",
+    "url": "/reactjs-calculus/static/js/2.f4de6a0b.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/reactjs-calculus/static/js/2.f4de6a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f8c5eaa7aced0d4199",
+    "url": "/reactjs-calculus/static/js/main.e122b1e9.chunk.js"
+  },
+  {
+    "revision": "863e66b3a95fded751df",
+    "url": "/reactjs-calculus/static/js/runtime-main.40e57d93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-calculus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0482e8d85aba761198907a8df16c1895",
+    "url": "/reactjs-calculus/static/media/sfx_right.0482e8d8.wav"
+  },
+  {
+    "revision": "805998a98cc9043c2999b887291f18b0",
+    "url": "/reactjs-calculus/static/media/sfx_swipe.805998a9.wav"
+  },
+  {
+    "revision": "2d714f36772e2938a9b4c90a574c223e",
+    "url": "/reactjs-calculus/static/media/sfx_wrong.2d714f36.wav"
+  }
+]);
